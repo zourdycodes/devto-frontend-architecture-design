7eb9ed75181a15aaa3da0b34d9a7a1b6ee1e49af
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-03 20:56:53 +0700

    :sparkles: styling up the header and navigation component
    checked

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { FaDev } from 'react-icons/fa';
+import { BiMessageRoundedCheck } from 'react-icons/bi';
+import { RiNotificationLine } from 'react-icons/ri';
 
 export const Navigation = () => {
   return (
-    <div>
-      <h1>Navigation</h1>
-    </div>
+    <header className="header">
+      <div className="headerContainer">
+        <a href="https://dev.to" className="headerContainer__logo">
+          <FaDev size="3.125rem" />
+        </a>
+
+        <div className="headerContainer__searchBox">
+          <form>
+            <input type="text" aria-label="search" placeholder="Search..." />
+          </form>
+        </div>
+
+        <div className="headerContainer__right">
+          <button>Write a post</button>
+          <i>
+            <BiMessageRoundedCheck />
+          </i>
+
+          <i>
+            <RiNotificationLine />
+          </i>
+
+          <span>
+            <img src="https://picsum.photos/200" alt="profile logo" />
+          </span>
+        </div>
+      </div>
+    </header>
   );
 };
