ad6789eabc6abf05ab65be2d5b19680ddc087fa0
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-03 14:10:35 +0700

    :rotating_light: Fix compiler / linter warnings.
    configured the stylint for scss

--- src/App.js
+++ src/App.js
@@ -2,20 +2,8 @@ import './App.scss';
 
 export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Ignite the engine!!!</h1>
     </div>
   );
 };
