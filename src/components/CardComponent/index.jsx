87dec2538a80e29797932a8e543897e5e7e60cd2
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-04 21:52:30 +0700

    :triangular_flag_on_post: build rightBar components
    done and finished

--- src/components/CardComponent/index.jsx
+++ src/components/CardComponent/index.jsx
@@ -1,9 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
-export const CardComponent = () => {
+export const CardComponent = ({ tag, articles }) => {
   return (
-    <div>
-      <h1>Card Components</h1>
+    <div className="card">
+      <header>
+        <h3>{tag}</h3>
+        {tag === 'Listings' && (
+          <a href="#">
+            <small>see all</small>
+          </a>
+        )}
+      </header>
+
+      <ul>
+        {articles.map((content) => {
+          return (
+            <li key={content.id}>
+              <a href="/#">{content.mainTitle}</a> <br />
+              <small>{content.subText}</small>
+              {content.newarticle && <span>new</span>}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
--- src/components/RightSidebar/index.jsx
+++ src/components/RightSidebar/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 
+import { news, help, listings } from '../../tempData';
+import { CardComponent } from '../CardComponent';
+
 export const RightSidebar = () => {
   return (
-    <div>
-      <h1>Right Sidebar</h1>
-    </div>
+    <aside className="rightBar">
+      <div className="rightBar__card-hackathon">
+        <p>
+          <img src="https://picsum.photos/200/300" alt="" />
+        </p>
+        <h2>
+          <a href="/#">"Hack the Planet" with New Relic & DEV</a>
+        </h2>
+
+        <p>
+          Use New Relic to build a climate-change observability app for the
+          chance to win up to $5,000!
+          <strong>
+            <a href="/#">&nbsp;join the hackathon</a>
+          </strong>
+        </p>
+      </div>
+
+      <CardComponent tag="Listings" articles={listings} />
+
+      <CardComponent tag="#news" articles={news} />
+
+      <CardComponent tag="#help" articles={help} />
+    </aside>
   );
 };
