d257a42c4f032f0753b6e887defc9e8cf35af901
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-09-05 09:59:36 +0700

    :boom: build the content components
    on-going

--- src/components/ArticleComponent/index.jsx
+++ src/components/ArticleComponent/index.jsx
@@ -1,9 +1,107 @@
 import React from 'react';
 
-export const ArticleComponent = () => {
+import { BiHeart } from 'react-icons/bi';
+import { FaRegComment } from 'react-icons/fa';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+export const ArticleComponent = ({
+  title,
+  cover_image,
+  tag_list,
+  url,
+  comments_count,
+  positive_reactions_count,
+  public_reactions_count,
+  user,
+  published_at,
+}) => {
   return (
-    <div>
-      <h1>Article Components</h1>
-    </div>
+    <article className="article">
+      {cover_image && (
+        <a
+          href={url}
+          className="article__image"
+          style={{
+            backgroundImage: `url(${cover_image})`,
+          }}
+        >
+          &nbsp;
+        </a>
+      )}
+
+      <div className="article__details">
+        <div className="u-pic">
+          <img src={user.profile_image_90} alt={title} />
+        </div>
+
+        <div className="u-details">
+          <a href={`https://dev.to/${user.username}`}>
+            <span className="u-name">{user.username}</span>
+          </a>
+          <a href={url}>
+            <span className="time">March 01, (One Day Ago)</span>
+          </a>
+
+          <a href={url}>
+            <h3>{title}</h3>
+          </a>
+
+          <div className="tags">
+            {tag_list?.map((tag, index) => {
+              return (
+                <a key={index} href={`https://dev.to/t/${tag}`}>
+                  <span className="tag">#{tag}</span>
+                </a>
+              );
+            })}
+          </div>
+
+          <div className="additional-details">
+            <div className="reactions">
+              {public_reactions_count + positive_reactions_count > 0 && (
+                <a href={url}>
+                  <span>
+                    <i>
+                      <BiHeart />
+                    </i>
+                    &nbsp;
+                    {public_reactions_count + positive_reactions_count} &nbsp;
+                    <span className="hidden-mobile">reactions</span>
+                  </span>
+                </a>
+              )}
+
+              <a href={url}>
+                <span>
+                  <i>
+                    <FaRegComment />
+                  </i>
+                  &nbsp;
+                  {comments_count > 0 ? (
+                    <span>
+                      {comments_count} &nbsp;
+                      <span className="hidden-mobile">comments</span>
+                    </span>
+                  ) : null}
+                  {comments_count === 0 ? (
+                    <span>
+                      <span className="show-mobile">{comments_count}</span>
+                      &nbsp;
+                      <span className="hidden-mobile">Add comment</span>
+                    </span>
+                  ) : null}
+                </span>
+              </a>
+            </div>
+
+            <div className="save">
+              <small>1 min read</small>
+              <button>save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
   );
 };
--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { ArticleComponent } from '../ArticleComponent';
 
 export const Content = () => {
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsloading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      try {
+        const response = await fetch('https://dev.to/api/articles');
+
+        if (!response.ok && response.status >= 400) {
+          throw new Error(
+            'Please check your connection or contact the IT support for further guidance'
+          );
+        }
+
+        const data = await response.json();
+
+        setArticles(data);
+        setIsloading(false);
+        console.log(data);
+      } catch (error) {
+        setIsloading(false);
+        throw new Error(error);
+      }
+    };
+
+    const debounceCallback = setTimeout(() => {
+      fetchArticles();
+    }, 2000);
+
+    return () => clearTimeout(debounceCallback);
+  }, []);
+
   return (
-    <div>
-      <h1>Content Component</h1>
-    </div>
+    <main className="main-content">
+      <header>
+        <h1>Posts</h1>
+        <nav>
+          <a href="/#">Feed</a>
+          <a href="/#">Week</a>
+          <a href="/#">Month</a>
+          <a href="/#">Infinity</a>
+          <a href="/#">Latest</a>
+        </nav>
+      </header>
+
+      <div className="article">
+        {articles?.map((article, index) => {
+          return <ArticleComponent key={index} {...article} />;
+        })}
+      </div>
+    </main>
   );
 };
